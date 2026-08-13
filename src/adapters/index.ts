59183fd39308689ed887947355532f0a3ff66738
Eddie-LowcountryDigitalWorks <eddie@lowcountrydigitalworks.com>
2026-08-12 21:43:15 -0400

    Export AccessGrant crypto and clock adapters

--- src/adapters/index.ts
+++ src/adapters/index.ts
@@ -1,3 +1,5 @@
 export * from "./in-memory-protected-content-store.js";
 export * from "./in-memory-workflow-store.js";
+export * from "./system-clock.js";
+export * from "./web-crypto-access-grant-secret.js";
 export * from "./web-crypto-id-generator.js";
